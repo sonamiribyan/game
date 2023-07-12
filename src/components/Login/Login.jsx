35d141cdcf7dc7d4420b006398a92569dcd7d5d7
GagoMartirosyan <martirosyangagik033@gmail.com>
2023-07-12 17:52:39 +0400

    login

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './login.css';
+
+const Login_form = () => {
+    return (
+        <div className="login-bg">
+            <p className="text">Login</p>
+            <input type="password" placeholder="Write Your Password"/>
+        </div>
+    );
+}
+
+export default Login_form;
